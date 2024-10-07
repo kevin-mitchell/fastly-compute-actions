7f96085439c24c48cb9abdef8b5e6cbea66bd118
Kevin Mitchell <>
2024-10-07 15:11:22 +0900

    test to see if we can spit the hash file output to a file

---
+++ hash-files/index.js
@@ -0,0 +1,26 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+const checkBin = require("../util/bin");
+
+const verbose = core.getBooleanInput("verbose");
+const skipBuild = core.getBooleanInput("skipBuild");
+
+checkBin("fastly", "version")
+  .then(() => {
+    let params = [
+      "compute",
+      "hash-files",
+      "> pkg/hash.txt",
+      "--non-interactive",
+    ];
+    if (verbose) params.push("--verbose");
+    if (skipBuild) params.push("--skip-build");
+
+    return exec.exec("fastly", params, {
+      cwd: core.getInput("project_directory"),
+    });
+  })
+  .catch((err) => {
+    core.setFailed(err.message);
+  });
