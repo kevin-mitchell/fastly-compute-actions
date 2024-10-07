7656e294e4daab8abdeef872a8d3ed2ba6e4e2fa
Kevin Mitchell <>
2024-10-07 15:48:52 +0900

    try to write hash again

--- hash-files/index.js
+++ hash-files/index.js
@@ -1,5 +1,7 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
+const glob = require("@actions/glob");
+const path = require("path");
 
 const checkBin = require("../util/bin");
 
@@ -7,14 +9,18 @@ const verbose = core.getBooleanInput("verbose");
 const skipBuild = core.getBooleanInput("skipBuild");
 
 checkBin("fastly", "version")
-  .then(() => {
+  .then(async () => {
     let params = ["compute", "hash-files", "--non-interactive"];
     if (verbose) params.push("--verbose");
     if (skipBuild) params.push("--skip-build");
 
-    return exec.exec("fastly", params, {
+    const result = await exec.getExecOutput("fastly", params, {
       cwd: core.getInput("project_directory"),
     });
+
+    console.log(result);
+
+    return exec.exec("echo", [result.stdout, "> pkg/hash.txt"], {});
   })
   .catch((err) => {
     core.setFailed(err.message);
